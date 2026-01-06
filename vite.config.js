35dcbacb38d379261ff25cce12f7cc307cdc9b69
Viswanath jagaluri <viswanathreddy@Viswanaths-MacBook-Pro.local>
2026-01-06 14:04:09 -0500

    kk

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: '/Portfolio/',
+  base: '/portfolio/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
